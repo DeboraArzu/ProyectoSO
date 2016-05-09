cc355031650a5d930083cc773f35a53618c97eb1
DeboraArzu <deboranise29@hotmail.com>
2016-05-09 13:50:05 -0600

    Add files via upload

---
+++ ProyectoFinal/ProyectoFinal/Productor.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ProyectoFinal
+{
+    class Productor : absWorker
+    {
+        public static Semaphore producir;
+        public static int size = 0;
+        public Productor(int id, int total, Command cmd) : base(id, total, cmd)
+        {
+        }
+
+        public override void work()
+        {
+            while (used < total)
+            {
+                working = false;
+                while (TheadPool.comando.Count != 0 && TheadPool.getInstance().consumidores.Count != 0) ;
+                while (used < total && size < TheadPool.maxSize)
+                {
+                    TheadPool.semaforo.WaitOne();
+                    producir.WaitOne();
+                    working = true;
+                    TheadPool.comando.Add(cmd);
+                    Thread.Sleep(100);
+                    producir.Release();
+                    TheadPool.semaforo.Release();
+
+                    used++;
+                    size++;
+                }
+                int aux = TheadPool.comando.Count;
+                for (int i = 0; i < aux; i++)
+                {
+                    absWorker consumer = new Consumidor(TheadPool.getInstance().consumidores.Count + TheadPool.getInstance().productores.Count);
+                    TheadPool.getInstance().consumidores.Add(consumer);
+                }
+            }
+            TheadPool.getInstance().productores.Remove(this);
+        }
+    }
+}
---
+++ ProyectoFinal/ProyectoFinal/TheadPool.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace ProyectoFinal
+{
+    class TheadPool
+    {
+        // public MySqlConnection sqlcon;
+        //string myConnectionString = "server=127.0.0.1;uid=root;" + "pwd=summerparadise21;database=so;";
+
+        MySqlConnection connection = null;
+        MySqlConnectionStringBuilder builder = new MySqlConnectionStringBuilder();
+
+
+        public List<absWorker> productores;
+        public List<absWorker> consumidores;
+
+        public static int maxSize;
+        public static List<Command> comando;
+        public static Semaphore semaforo;
+
+        private static TheadPool _instance = null;
+
+        private TheadPool(int producerParam, int consumerParam, int commandsSize)
+        {
+            comando = new List<Command>();
+            semaforo = new Semaphore(producerParam + consumerParam, producerParam + consumerParam);
+
+            this.productores = new List<absWorker>();
+            this.consumidores = new List<absWorker>();
+
+            Productor.producir = new Semaphore(producerParam, producerParam);
+            Consumidor.consume = new Semaphore(consumerParam, consumerParam);
+
+            maxSize = commandsSize;
+
+            try
+            {
+                builder.Server = "127.0.0.1";
+                builder.UserID = "root";
+                builder.Password = "summerparadise21";
+                builder.Database = "so";
+                connection = new MySqlConnection(builder.ToString());
+                connection.Open();
+                
+            }
+            catch (MySqlException ex)
+            {
+                bool f = false;
+            }
+        }
+
+        public static TheadPool getInstance(int productor, int consumidor, int cola)
+        {
+            if (_instance == null)
+                _instance = new TheadPool(productor, consumidor, consumidor);
+            return _instance;
+        }
+        public static TheadPool getInstance()
+        {
+            if (_instance == null)
+                _instance = new TheadPool(3, 5, 10);
+            return _instance;
+        }
+
+        public void addRegister(string origen, string destino, int cantidad)
+        {
+            //SQL INSERT
+            ConexionSQL c = new ConexionSQL();
+            absWorker producer = new Productor(productores.Count + consumidores.Count, cantidad, new InsertionCommand(origen, destino));
+            c.insert(origen, destino);
+            productores.Add(producer);
+        }
+
+        public void removeRegister(string origen, string destino, int cantidad)
+        {
+            //SQL DELETE
+            absWorker consumer = new Productor(productores.Count + consumidores.Count, cantidad, new DeleteCommand(origen, destino));
+            productores.Add(consumer);
+        }
+    }
+}
